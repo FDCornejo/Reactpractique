5fd377699f48a4b245aa576d4564e42547a1e8d5
DanielCornejo <fdanielcm@gmail.com>
2019-09-26 02:07:31 -0500

    Estructura ya hecha

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Link, Router } from "react-router-dom";
 
 function App() {
   return (
 
 
     <div>
-
-      <div style={{ flexDirection: "row" }}>
-        <a style={{ margin: 5 }} to="/">Home</a>
-      </div>
+<h1>Este es mi Home</h1>
 
     </div>
 
--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -2,24 +2,25 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Parent from '../Views/Example';
 import App from '../App';
+import '../Router/router.css'
 
 
 export default class AppRouter extends Component {
     render() {
         return (
             <Router>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <a className="navbar-brand" to="/">Navbar</a>
+                <nav className="navbar navbar-expand-lg navbar-dark">
+                    <a className="navbar-brand text-light " to="/">La APP de Cornejo</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <Link style={{ margin: 5 }} to="/">Home</Link>
+                                <Link className="nav-link" style={{ margin: 5 }} to="/">Home</Link>
                             </li>
                             <li className="nav-item">
-                                <Link style={{ margin: 5 }} to="/props-example">Props Example</Link>
+                                <Link className="nav-link" style={{ margin: 5 }} to="/props-example">Props Example</Link>
 
                             </li>
                         </ul>
